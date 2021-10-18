7481ba850e384831eb5ff96bb176edff85bb13c1
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2021-10-18 17:18:50 +0100

    improve core notification logging

--- ui/page/dexclient/listeners.go
+++ ui/page/dexclient/listeners.go
@@ -2,6 +2,7 @@ package dexclient
 
 import (
 	"fmt"
+	"sync/atomic"
 
 	"decred.org/dcrdex/client/core"
 	"decred.org/dcrdex/dex/msgjson"
@@ -15,11 +16,11 @@ func (pg *Page) connectDex(h string, password []byte) {
 }
 
 func (pg *Page) updateOrderBook() {
-	orderBoook, err := pg.Dexc.Book(pg.selectedMaket.host, pg.selectedMaket.marketBaseID, pg.selectedMaket.marketQuoteID)
+	orderBook, err := pg.Dexc.Book(pg.selectedMaket.host, pg.selectedMaket.marketBaseID, pg.selectedMaket.marketQuoteID)
 	if err != nil {
 		return
 	}
-	pg.orderBook = orderBoook
+	pg.orderBook = orderBook
 	pg.miniTradeFormWdg.orderBook = pg.orderBook
 }
 
@@ -48,22 +49,38 @@ func (pg *Page) listenerMessages() {
 	}
 }
 
+var listening uint32
+
 // readNotifications reads from the Core notification channel.
+// TODO: Why is this called more than once causing duplicate logs?
 func (pg *Page) readNotifications() {
+	if !atomic.CompareAndSwapUint32(&listening, 0, 1) {
+		return
+	}
 	ch := pg.Dexc.NotificationFeed()
 	for {
 		select {
 		case n := <-ch:
-			fmt.Println("Recv notification", n)
-			fmt.Println("<INFO>", n.ID())
-			fmt.Println("<INFO>", n.Severity())
-			fmt.Println("<INFO>", n.Type())
-			fmt.Println("<INFO>", n.DBNote())
-			fmt.Println("<INFO>", n.String())
-			fmt.Println("<INFO>", n.Subject())
+			switch n.Type() {
+			case core.NoteTypeBalance:
+				break
 
-			if n.Type() == core.NoteTypeFeePayment {
+			case core.NoteTypeWalletState:
+				wallet := n.(*core.WalletStateNote).Wallet
+				fmt.Printf("== %s wallet synced = %t, sync progress = %f ==\n", wallet.Symbol, wallet.Synced, wallet.SyncProgress)
+
+			case core.NoteTypeFeePayment:
 				pg.RefreshWindow()
+				fallthrough
+
+			default:
+				fmt.Println("Recv notification", n)
+				// fmt.Println("<INFO>", n.ID())
+				// fmt.Println("<INFO>", n.Severity())
+				// fmt.Println("<INFO>", n.Type())
+				// fmt.Println("<INFO>", n.DBNote())
+				// fmt.Println("<INFO>", n.String())
+				// fmt.Println("<INFO>", n.Subject())
 			}
 			pg.refreshUser()
 		case <-pg.ctx.Done():
